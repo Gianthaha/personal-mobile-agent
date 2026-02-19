d8c40a290a0733b3af41719de34c98414f26e719
Gianthaha <shuoxin_wang@163.com>
2026-02-20 03:24:05 +0800

    test

--- scripts/run_tasks_evolution.sh
+++
@@ -1,7 +0,0 @@
-
-TASK_JSON_PATH="data/custom_tasks_example.json"
-SETTING="evolution"
-python run.py \
-    --run_name "example-user1" \
-    --setting user1 \
-    --tasks_json "data/user1.json"
--- verify/__init__.py
+++
@@ -1,3 +0,0 @@
-from .mobile_verify import verify_func, MobileVerifier
-
-__all__ = ['verify_func', 'MobileVerifier']
